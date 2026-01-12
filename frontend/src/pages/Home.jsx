8cca8e6f9de8da50961c2c85cb9e93f35ea78059
vsirish123 <vsirish99@gmail.com>
2026-01-12 22:58:43 +0530

    Added Home.jsx page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import FilterBar from "../components/FilterBar";
+import { sampleVideos } from "../utils/sampleVideos";
+import { useState } from "react";
+
+function Home()
+{
+    const [collapsed,setCollapsed]=useState(false);
+    const [category,setCategory]=useState("All");
+    const [searchText,setSearchText]=useState("");
+
+    const categoryFiltered=category==="All"? sampleVideos:sampleVideos.filter((v)=>v.category===category);
+
+    const finalVideos=categoryFiltered.filter((v)=>v.title.toLowerCase().includes(searchText.toLowerCase()));
+
+    return (
+        <>
+            <Header toggleSidebar={()=>setCollapsed(!collapsed)} setSearchText={setSearchText}/>
+            <Sidebar collapsed={collapsed}/>
+            <div className={`main ${collapsed ? "main-collapsed":""}`}>
+                <FilterBar setCategory={setCategory}/>
+                <div className="video-grid">
+                    {finalVideos.map(()=>{
+                    
+                    })}
+                </div>
+            </div>
+        </>
+    )
+
+}
+export default Home;
