a601608879b5c2cad5e80d8779e1840459c3fc8d
vsirish123 <vsirish99@gmail.com>
2026-01-12 19:27:30 +0530

    Added sidebar.jsx in components

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { AiFillHome,AiOutlineClockCircle,AiOutlineLike,AiShorts } from "react-icons/ai";
+import { MdSubscriptions,MdOutlineVideoLibrary } from "react-icons/md";
+import { FaUserCircle } from "react-icons/fa";
+function Sidebar(){
+    return (
+        <div>
+            <div className="sidebar-item">
+                <AiFillHome className="sidebar-icon"/>
+            </div>
+            <div className="sidebar-item">
+                <MdSubscriptions className="sidebar-icon"/>
+            </div>
+            <div className="sidebar-item">
+                <AiShorts className="sidebar-icon"/>
+            </div>
+            <div className="sidebar-item">
+                <FaUserCircle className="sidebar-icon"/>
+            </div>
+        </div>
+
+    )
+}
+export default Sidebar;
