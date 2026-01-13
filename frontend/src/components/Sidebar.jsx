837e4c0ac8c5621177369b5e381aee391ab4a99d
vsirish123 <vsirish99@gmail.com>
2026-01-13 22:27:47 +0530

    updated Sidebar when i click on home it goes to home page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { AiFillHome,AiOutlineClockCircle,AiOutlineLike,AiShorts } from "react-icons/ai";
 import { MdSubscriptions} from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 function Sidebar(){
+    const navigate=useNavigate();
     return (
         <div>
-            <div className="sidebar-item">
+            <button className="sidebar-item" onClick={()=>navigate("/")}>
                 <AiFillHome className="sidebar-icon"/>
-            </div>
+            </button>
             <div className="sidebar-item">
                 <MdSubscriptions className="sidebar-icon"/>
             </div>
