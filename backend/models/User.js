8275390f2d10221ecc71e7984faf0a6ac81c226d
vsirish123 <vsirish99@gmail.com>
2026-01-17 23:48:22 +0530

    Addded userschema in user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,11 @@
 import mongoose from "mongoose";
 
-const
+const userSchema=new mongoose.Schema(
+    {
+        username:{type:String,required:true},
+        email:{type:String,required:true},
+        password:{type:String,required:true}
+    }
+);
+
+export default mongoose.model("User",userSchema);
