a10450aeaccd3cec80bf53ab2b898a773737c7f7
vsirish123 <vsirish99@gmail.com>
2026-01-20 23:56:54 +0530

    Added edit comment and deletecomment functions

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -46,8 +46,53 @@ export const editComment=async(req,res)=>{
 
         if(!comment)
         {
-            return res.json("comment not found");
+            return res.json({message:"comment not found"});
         }
         
+        if(comment.userId!=userId)
+        {
+            return res.json({message:"unauthorised"});
+        }
+
+        const updated=await comment.findByIdAndUpdate(
+            commentId,
+            {text:req.body.text},
+            {new :true}
+        )
+        
+        res.json({message:"updated comment",comment:updated})
+    }
+    catch(err)
+    {
+        res.json({err:err.message});
+
+    }
+}
+
+export const deleteComment=async(req,res)=>{
+    try{
+        const {commentId}=req.params;
+        const userId=req.user.id;
+
+        const comment=await Comment.findById(commentId);
+
+        if(!comment)
+        {
+            return res.json({message:"comment not found"});
+        }
+        
+        if(comment.userId!=userId)
+        {
+            return res.json({message:"unauthorised"});
+        }
+
+        const updated=await comment.findByIdAndDelete(commentId);
+        
+        res.json({message:"comment deleted"})
+    }
+    catch(err)
+    {
+        res.json({err:err.message});
+
     }
 }
