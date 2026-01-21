922157777c11c3228702bcc3c31eefa39d201514
vsirish123 <vsirish99@gmail.com>
2026-01-21 18:58:56 +0530

    updated server.js, added routes in server.js for app routing

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import {authRoutes} from "./routes/authRoutes.js"
+import {channelRoutes} from "./routes/channelRoutes.js"
+import {videoRoutes} from "./routes/videoRoutes.js"
+import {channelRoutes} from "./routes/commentRoutes.js"
 
 const app=express();
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/youtube-info").then(()=>console.log("Mongodb connected"));
+
+app.use("/api/auth",authRoutes);
+app.use("/api/channel",channelRoutes);
+app.use("/api/videos",videoRoutes);
+app.use("/api/comments",commentRoutes);
 
 app.get("/",(req,res)=>{
     res.send("youtube clone Backend Running...");
