76fb7d692ed8b8a3472df6ace2485cbc143a9dd7
vsirish123 <vsirish99@gmail.com>
2026-01-21 18:26:22 +0530

    Added getvideosbychannel and getvideobyid functions for getting videos

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -28,3 +28,33 @@ export const uploadVideo=async(req,res)=>{
         res.status(500).json({err:err.message});
     }
 }
+
+
+export const getVideoByChannel=async(req,res)=>{
+    try{
+        const {channelId}=req.params;
+        const videos=await Video.find({channelId});
+        res.json(videos);
+    }
+    catch(err)
+    {
+        res.status(500).json({err:err.message});
+    }
+}
+
+export const getVideoById=async(req,res)=>{
+    try{
+        const video=await Video.findById(req.params.id);
+
+        if(!video)
+        {
+            return res.json({message:"video not found"});
+        }
+
+        res.json(video);
+    }
+    catch(err)
+    {
+        res.status(500).json({err:err.message});
+    }
+}
