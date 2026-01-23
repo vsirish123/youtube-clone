e0a1cff1030eb42533776b290486280af60ab49a
vsirish123 <vsirish99@gmail.com>
2026-01-23 22:59:15 +0530

    Added css for filterbar

--- frontend/src/components/FilterBar.jsx
+++ frontend/src/components/FilterBar.jsx
@@ -1,9 +1,9 @@
 const filters=["All","React","JavaScript","Music","Gaming","Sports","Technology"];
 function FilterBar({setCategory}){
     return (
-        <div style={{display:"flex",gap:10,marginBottom:20}}>
+        <div className="filter-bar">
             {filters.map((f)=>(
-                <button className="filter-btn" key={f} onClick={()=>setCategory(f)} style={{padding:"8px 15px",borderRadius:20,border:"1px solid #ccc",cursor:"pointer"}}>
+                <button className="filter-btn" key={f} onClick={()=>setCategory(f)}>
                     {f}
                 </button>
         ))}
