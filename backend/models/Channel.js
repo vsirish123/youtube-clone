46096ae1e85026954683bd60478de8c426f73b46
vsirish123 <vsirish99@gmail.com>
2026-01-30 18:08:40 +0530

    updated channelmodels

--- backend/models/Channel.js
+++ backend/models/Channel.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const channelSchema=new mongoose.Schema(
     {
         channelName:{type:String,required:true},
-        owner:{type:String,required:true},
+        owner:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
         description:{type:String,default:""},
         channelBanner:{type:String,default:""},
         subscribers:{type:Number,default:0},
