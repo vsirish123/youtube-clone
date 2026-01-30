b8f54abacf18438217b6b3ffe75016fa0332c1f2
vsirish123 <vsirish99@gmail.com>
2026-01-30 19:33:17 +0530

    updated app.jsx routes for routin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import VideoPage from "./pages/VideoPage";
+import UploadVideo from "./pages/UploadVideo";
+import ChannelPage from "./pages/ChannelPage";
+import CreateChannel from "./pages/CreateChannel"
+import EditVideo from "./pages/EditVideo"
 function App()
 {
     return (
         <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/login" element={<Login />}/>
             <Route path="/register" element={<Register />}/>
-            <Route path="/video/:id" element={<VideoPage />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/video/:id" element={<VideoPage/>}/>
+            <Route path="/create-channel" element={<CreateChannel />}/>
+            <Route path="/channels/:id" element={<ChannelPage />}/>
+            <Route path="/upload-video/:channelId" element={<UploadVideo />}/>
+            <Route path="/edit-video/:videoId" element={<EditVideo />}/>
         </Routes>
     )
 }
